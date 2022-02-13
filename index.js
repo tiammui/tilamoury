2018557c59b11a5153178e269761fbf04bd08b75
Tiamiyu Muizz <48317927+tiammui@users.noreply.github.com>
2022-02-13 04:25:40 +0000

    irr

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const mySecret = process.env['KEYY']
 console.log(mySecret)
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static('public'));
+app.listen(3000, () => {
+  console.log('Server running at port ', 3000);
+});
